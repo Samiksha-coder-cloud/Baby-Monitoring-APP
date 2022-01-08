53239a750af74526026753030e23b4f0e92338d8
Samiksha-coder-cloud <78074734+Samiksha-coder-cloud@users.noreply.github.com>
2022-01-08 16:25:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ objects = "";
 status1 = "";
 
 function preload() {
- 
+
+    alarm = loadSound("alarm.mp3");
 }
 
 function setup() {
@@ -24,12 +25,17 @@ function modelLoaded( ) {
 function draw() {
     image(video, 0, 0, 400, 380);
 
-    objectDetector.detect(video, gotResult);
+    if (objects == "person") {
 
-    if (objects.label == "person") {
-        for (i = 0; i < objects[i].label; i++) {
-            document.getElementById("object").innerHTML = "Baby Found";
+        objectDetector.detect(video, gotResult);
+
+        for (i = 0; i < results[i].label; i++) {
+
             document.getElementById("Status").innerHTML = "Status: Objects Detected";
+            document.getElementById("object").innerHTML = "Baby Not Found";
+
+            alarm.play();
+            
             fill("#ff0000");
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + " % ", objects[i].x + 15, objects[i].y + 15);
@@ -37,9 +43,6 @@ function draw() {
             stroke("#ff0000");
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         }
-    }
-    else {
-        document.getElementById("object").innerHTML = "Baby Not Found";
     }
 }
 
